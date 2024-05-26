b47a838a41c3b0b7f1f7d26d551efefc89ae2063
Konstantin Pershin <kostya.pershin.18@mail.ru>
2024-05-26 16:45:59 +0500

    add db, migrations, register

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 # from fastapi_jsonrpc import API, JsonRpcRouter
+from src.auth.auth_router import router as auth_router
 
 from config.config import CORS_ORIGINS
 
@@ -18,6 +19,11 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.include_router(
+    auth_router,
+    prefix="/auth"
 )
 
 @app.get("/")

5e3b8b2454d13c2f33afd0ae7d4d7d84b0e78689
pribishlove <m.makushkin2004@gmail.com>
2024-05-29 11:04:46 +0500

    update 3

---
+++ Booking MySelf/src/LoginForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, message } from 'antd';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      const response = await axios.post('http://localhost/api/auth/login', {
+        email: values.email,
+        password: values.password,
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      message.success('Авторизация прошла успешно!');
+    } catch (error) {
+      message.error('Ошибка авторизации!');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Form name="login" onFinish={onFinish} layout="vertical">
+      <Form.Item
+        name="email"
+        label="Почта"
+        rules={[{ required: true, message: 'Пожалуйста, введите почту!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label="Пароль"
+        rules={[{ required: true, message: 'Пожалуйста, введите пароль!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Войти
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;

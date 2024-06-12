6a73820649e7733a5af0a3e00e3e07ac28b5fc05
Konstantin Pershin <kostya.pershin.18@mail.ru>
2024-06-13 03:55:47 +0500

    add cancelling uncongirmed bookings within an hour

--- backend/src/services/celery_service.py
+++ backend/src/services/celery_service.py
@@ -1,11 +1,25 @@
 from pydantic import EmailStr
-from celery import Celery
-from datetime import datetime
+from celery import Celery, current_app
+from celery.signals import worker_ready
+from celery.schedules import crontab
+from datetime import datetime, timedelta
+from src.utils.convert_time import convert_time
 from src.services.email_service import EmailService
+from src.models.bookings import booking
+from sqlalchemy import select, and_, update
 from config.config import REDIS_PORT
+from config.database import async_session_maker
+import asyncio
 
 
 celery = Celery('task', broker=f'redis://redis:{REDIS_PORT}/0', result_backend=f'redis://redis:{REDIS_PORT}/0')
+
+celery.conf.beat_schedule = {
+    'cancel-pending-bookings-every-hour': {
+        'task': 'src.services.celery_service.cancel_expired_pending_bookings',
+        'schedule': crontab(minute=0, hour='*'),
+    },
+}
 
 @celery.task
 def send_email_task(
@@ -30,4 +44,32 @@ def send_email_task(
     except Exception as e:
         print(e)
         return str(e)
-    
+
+
+@celery.task
+def cancel_expired_pending_bookings():
+    asyncio.run(cancel_expired_pending_bookings_async())
+
+
+async def cancel_expired_pending_bookings_async():
+    async with async_session_maker() as session:
+        delta_time =  convert_time(datetime.now()) - timedelta(hours=1)
+        
+        query = select(booking).where(
+            and_(
+                booking.c.status == 'pending',
+                booking.c.created_at < delta_time
+            )
+        )
+
+        pending_bookings = (await session.execute(query)).fetchall()
+
+        for pending_booking in pending_bookings:
+            await session.execute(
+                update(booking)
+                .where(booking.c.id == pending_booking.id)
+                .values(status='cancelled')
+            )
+
+        await session.commit()
+        return {'status': True}
